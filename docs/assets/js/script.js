97bf3b61209efc1630956ad65f63140e24a5bd48
john.a.clark3 <john.a.clark3@gmail.com>
2022-01-01 14:29:21 -0700

    get length function

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,23 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
 // Write password to the #password input
 function writePassword() {
-  //ask length
-  var passwordLength = window.prompt("How many characters would you like your password to be?  Select a number between 8 and 10.");
-  if (passwordLength == 8) {
-    window.alert("Yo, this is 8 now");
-  }
-  else if (passwordLength == 9) {
-    window.alert("Yo Yo, this is 9 now");
-  }
-  else if (passwordLength == 10) {
-    window.alert("Yo Yo Yo, this is 10 now");
-  }
 
-  else {
-    window.alert("Are you fluffing kidding me??  Try again!!!");
-  }
+  
+  var length = getLength();
+  var lower = getLower();
+  var upper = getUpper();
+  var symbol = getSymbol();
+  
+
+  
   
     
 
@@ -31,3 +27,13 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function getLength() {
+  var passwordLength = window.prompt("How many characters would you like your password to be?  Select a number between 8 and 128.");
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Pease enter a valid response.");
+    return getLength();
+  }
+  return passwordLength;
+}
+
