5db0fadb88d7b92caa908f8a8bf0acd49c129e96
john.a.clark3 <john.a.clark3@gmail.com>
2022-01-01 12:43:15 -0700

    made if statements

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -3,7 +3,26 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  //ask length
+  var passwordLength = window.prompt("How many characters would you like your password to be?  Select a number between 8 and 10.");
+  if (passwordLength == 8) {
+    window.alert("Yo, this shit is 8 now");
+  }
+  else if (passwordLength == 9) {
+    window.alert("Yo Yo, this shit is 9 now");
+  }
+  else if (passwordLength == 10) {
+    window.alert("Yo Yo Yo, this shit is 10 now");
+  }
+
+  else {
+    window.alert("Are you fluffing kidding me??  Try again!!!");
+  }
+  
+    
+
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
