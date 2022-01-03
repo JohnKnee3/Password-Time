46331976bd7865a09b6fe03356afa00eb2939dec
john.a.clark3 <john.a.clark3@gmail.com>
2022-01-02 20:53:07 -0700

    Update script.js

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -87,17 +87,21 @@ function getCharacter(functions) {
 
 //Go into randomly selected function and return character
 function getRandomLower() {
+  didRandomLower = true;
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 function getRandomUpper() {
+  didRandomUpper = true;
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
 function getRandomNumber() {
+  didRandomNumber = true;
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
 function getRandomSymbol() {
+  didRandomSymbol = true;
   const symbols= '!@#$%^&*(){}[]=<>/,.';
   return symbols [Math.floor(Math.random() * symbols.length)];
 }
